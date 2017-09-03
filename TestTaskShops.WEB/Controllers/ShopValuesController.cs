e72be6ed01a3d8be023e9bbe62d6f5fefe6ecda9
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-09-03 17:42:38 +0300

    Web Api + Ninject + EF

---
+++ TestTaskShops.WEB/Controllers/ShopValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using TestTaskShops.BLL.DTO;
+using TestTaskShops.BLL.Interfaces;
+
+namespace TestTaskShops.WEB.Controllers
+{
+    public class ShopValuesController : ApiController
+    {
+        private IShopService shopService;
+
+        public ShopValuesController(IShopService shopService)
+        {
+            this.shopService = shopService;
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            shopService.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TestTaskShops.WEB/Util/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+using Ninject;
+
+using TestTaskShops.BLL.Interfaces;
+using TestTaskShops.BLL.Services;
+
+namespace TestTaskShops.WEB.Util
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IShopService>().To<ShopService>();
+            kernel.Bind<IProductService>().To<ProductService>();
+        }
+    }
+}
