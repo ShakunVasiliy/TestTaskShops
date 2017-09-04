1c3ac588ea9210f684b0f491b5c517a3158c67aa
Vasiliy Shakun <shakun.vasiliy@gmail.com>
2017-09-04 06:02:57 +0300

    ShopId in ShopDTO

--- TestTaskShops.BLL/DTO/ProductDTO.cs
+++ TestTaskShops.BLL/DTO/ProductDTO.cs
@@ -7,5 +7,6 @@ namespace TestTaskShops.BLL.DTO
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int ShopId { get; set; }
     }
 }
